3576293460b916000a50692188b720f8942ea015
Luis Torres <98085987+luistorrescisneros@users.noreply.github.com>
2022-07-06 16:26:48 +0200

    merge feature (#6)

---
+++ lib/src/Calc.cpp
@@ -0,0 +1,17 @@
+/*
+ * Calc.cpp
+ *
+ *  Created on: 10 Feb 2020
+ *      Author: Andy Gibson
+ */
+
+
+#include "calc/Calc.h"
+
+int Calc::add(int a, int b) {
+	return a+b;
+}
+
+int Calc::sub(int a, int b) {
+	return a-b;
+}
