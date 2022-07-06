3b0a981ee7782618b5798967ba26784bafc5a72c
luistorrescisneros <luis.torres.cisneros@sioux.eu>
2022-07-06 15:38:39 +0200

    Remove retrieving artifacts

---
+++ lib/include/calc/Calc.h
@@ -0,0 +1,19 @@
+/*
+ * Calc.h
+ *
+ *  Created on: 10 Feb 2020
+ *      Author: Andy Gibson
+ */
+
+#ifndef CALCLIB_INCLUDE_CALC_CALC_H_
+#define CALCLIB_INCLUDE_CALC_CALC_H_
+
+class Calc {
+public:
+	int add(int a,int b);
+	int sub(int a,int b);
+};
+
+
+
+#endif /* CALCLIB_INCLUDE_CALC_CALC_H_ */
